445e1a378d25946eddf1cccdc8fc4d44c90678a1
unknown <onni09nevala@gmail.com>
2023-06-29 11:17:47 +0300

    first version!

---
+++ src/main/java/com/ctih/bors/Bors.java
@@ -0,0 +1,16 @@
+package com.ctih.bors;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Bors implements ModInitializer {
+	public static final String MOD_ID = "bors";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/ctih/bors/BorsClient.java
@@ -0,0 +1,11 @@
+package com.ctih.bors;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BorsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+}
